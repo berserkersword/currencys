b3afefba0b2612a440987588b1586af8058d8f35
berserkersword <lamjarred063@gmail.com>
2022-08-20 10:51:58 -0500

    main

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -5,6 +5,7 @@ import { Chart as ChartJS, registerables } from 'chart.js';
 ChartJS.register(...registerables)
 
 const Chartjs = (props: { chartData: ChartModel }) => {
+    console.log(props.chartData.datasets);
     return <Line data={props.chartData} />
 }
 
--- src/pages/Charts.tsx
+++ src/pages/Charts.tsx
@@ -5,30 +5,20 @@ import { useParams } from 'react-router-dom';
 import { ChartModel, ChartRes } from '../Model/Model';
 
 const Charts = () => {
-    const { id } = useParams()
+
+    const { id } = useParams();
+
     const [res, setRes] = useState<ChartRes>({
         prices: [
-            [1, 2]
+            [1660831328221, 23498.056141562774],
+            [1660834820925, 23428.465875440546],
+            [1660838500790, 23472.0421571502]
         ],
         total_volumes: [
             [1, 2]
         ],
         marcet_caps: [
             [1, 2]
-        ]
-    });
-
-    const [userData, setUserData] = useState<ChartModel>({
-        labels: res.prices.map(data => {
-            return data[0] / 3600
-        }),
-        datasets: [
-            {
-                label: 'Id',
-                data: res.prices.map(data => {
-                    return data[1]
-                })
-            }
         ]
     });
 
@@ -40,6 +30,17 @@ const Charts = () => {
         }
         Query()
     }, [id])
+    console.log(res);
+
+    let [userData, setUserData] = useState<ChartModel>({
+        labels: res.prices.map(data => data[0]),
+        datasets: [
+            {
+                label: `${id}`,
+                data: res.prices.map(date => date[1])
+            }
+        ]
+    });
 
     return (
         <div>
